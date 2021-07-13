3a79fa42d01d718761413e1ceae4c1a74ba0e9f4
EdwarTriana <edwarsteventriana@gmail.com>
2021-07-12 19:01:08 -0500

    actualizacion cursos

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -11,32 +11,32 @@ export class CursosComponent implements OnInit {
     {
       titulo:'Piano',
       descripcion: 'Aprende a interpretar este maravilloso instrumento, trae grandes beneficios contribuyendo en la mejora de la destreza mental y habilidades cognitivas, coordinacion e independencia del movimiento en el cuerpo.',
-      imgUrl:'../../../assets/img/imgCursos/pianoCurso.jpg'
+      imgUrl:'assets/img/imgCursos/pianoCurso.jpg'
     },
     {
       titulo:'Guitarra Acustica',
       descripcion:'Es un instrumento de cuerdas punzadas que trae beneficios de relajacion , estimulacion cerebral, aumento de la confianza y motivacion a la creatividad entre otros...',
-      imgUrl:'../../../assets/img/imgCursos/guitarraAcustica.jpg'
+      imgUrl:'/assets/img/imgCursos/guitarraAcustica.jpg'
     },
     {
       titulo:'Tecnica Vocal',
       descripcion:'Aprender a cantar es el sueño de muchas personas, por eso a traves de la tecnica vocal enseñamos diferentes procesos de interpretacion llevando la voz a un nuevo nivel impactando cualquier audiencia.',
-      imgUrl:'../../../assets/img/imgCursos/canto.jpg'
+      imgUrl:'assets/img/imgCursos/canto.jpg'
     },
     {
       titulo:'Guitarra Electrica',
       descripcion:'Es un instrumento deseado por grandes y pequeños el cual brinda grandes ventajas como la estimulacion cerebral, el aumento de la autoestima, concentracion y equilibrio emocional. ',
-      imgUrl:'../../../assets/img/imgCursos/guitarraElectrica.jpg'
+      imgUrl:'assets/img/imgCursos/guitarraElectrica.jpg'
     },
     {
       titulo:'Bateria',
       descripcion:'Es un instrumento de percusion admirado entre grandes y chicos, trae beneficios de coordinacion , concentracion , disciplina y confianza en si mismo.',
-      imgUrl:'../../../assets/img/imgCursos/bateria.jpg'
+      imgUrl:'assets/img/imgCursos/bateria.jpg'
     },
     {
       titulo:'Violin',
       descripcion:'Es un instrumento supremamente apasionante, ya que al aprender a interpretarlo se desarrollan capacidades como la memoria auditiva, el autocontrol, la disciplina y la expresion de nuestros sentimientos de una manera creativa y unica.',
-      imgUrl:'../../../assets/img/imgCursos/violin.jpg'
+      imgUrl:'assets/img/imgCursos/violin.jpg'
     }
   ]
   constructor() { }
